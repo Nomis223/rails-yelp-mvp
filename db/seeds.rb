de0306ba70223c92894d9576e12dad72837041ea
Simon Abraham <nomis223@gmail.com>
2018-02-14 12:00:04 +0100

    feat: restaurant and review models w/ validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create( name: 'Food Place', address: '2354 Another Blah Street', phone_number: '702-911-0001', category: 'chinese' ) 
+Restaurant.create( name: 'Burger Palace', address: '2345 Shit Lord Street', phone_number: '702-911-0002', category: 'italian' )
+Restaurant.create( name: 'We Sell Food', address: '2356 Blah Straße', phone_number: '49-2758-84967423', category: 'japanese')
+Restaurant.create( name: 'Running Out of Ideas', address: '2344 Place Street', phone_number: '702-911-0003', category: 'french')
+Restaurant.create( name: 'Almost Done', address: '2344 Blah Street', phone_number: '702-911-0000', category: 'belgian' )
+Restaurant.create( name: 'Finally Done', address: '8383 Street Place', phone_number: '856-746-9485', category: 'chinese' )
